bb3a7c98fc6bbae988a3684775010ac2b540369a
Jay Spears <j.spears.denver@gmail.com>
2020-11-24 23:04:51 -0700

    website building

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-#import necessary libraries
+  
+#import necessary libraries from flask import Flask, render_template, redirect
 from flask import Flask, render_template, redirect
 from flask_restful import Resource, Api, reqparse
 import os
 
+# import otherroute! which is another py file
 try:
+    # The typical way to import flask-cors
     from flask_cors import CORS
 except ImportError:
+    # Path hack allows examples to be run without installation.
     import os
     parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     os.sys.path.insert(0, parentdir)
 
     from flask_cors import CORS
 
-
-
 #create Flask app instance
 app = Flask(__name__)
 api = Api(app)
+# cors=CORS(app, resources=r'smallSst.json', allow_headers='Content-Type')
 
 @app.route("/")
-# @cross_origin()
 def index():
-    return render_template("indexTransit.html")
+    return render_template("index.html")
 
 @app.route("/timeSeriesFunding")
 def timeSeriesFunding():
@@ -31,10 +33,6 @@ def timeSeriesFunding():
 def naturalLanguage():
     return render_template("naturalLanguage.html")
 
-@app.route("/timeSeriesFunding")
-def timeSeriesFunding():
-    return render_template("timeSeriesFunding.html")
-
 @app.route("/transitAccesibility")
 def transitAccesibility():
     return render_template("transitAccesibility.html")
@@ -42,6 +40,10 @@ def transitAccesibility():
 @app.route("/fundingBySource")
 def fundingBySource():
     return render_template("fundingBySource.html")
+
+@app.route("/dataSources")
+def dataSources():
+    return render_template("dataSources.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port = 1123)
